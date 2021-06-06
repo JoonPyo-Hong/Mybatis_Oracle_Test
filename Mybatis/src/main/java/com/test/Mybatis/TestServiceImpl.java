06f897aa46e83fa71d8947c598ddebd1e639f5cb
Hong JUN PYO <wnsvy4231@naver.com>
2021-06-06 13:43:50 +0900

    backup

--- Mybatis/src/main/java/com/test/Mybatis/TestServiceImpl.java
+++ Mybatis/src/main/java/com/test/Mybatis/TestServiceImpl.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.test.Mybatis.mapper.Mapper;
-
 @Service
 public class TestServiceImpl implements TestService{
 
--- Mybatis/src/main/java/com/test/Mybatis/mapper/Mapper.java
+++
@@ -1,9 +0,0 @@
-package com.test.Mybatis.mapper;
-
-import java.util.List;
-
-import com.test.Mybatis.DTO;
-
-public interface Mapper {
-	public List<DTO> viewAll();
-}
